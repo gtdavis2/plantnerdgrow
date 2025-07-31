96e60a12be909ec20571a316349a46099999e1b2
Nathaniel Gallinger <nathaniel.gallinger@gmail.com>
2025-07-30 20:19:42 -0700

    Update fonts: Plant Nerd logo to Sarina, navigation and hamburger menu to Cotton Regular

--- script.js
+++ script.js
@@ -253,7 +253,7 @@ function initMobileMenu() {
 function initPerformanceOptimizations() {
     // Preload critical resources
     const criticalResources = [
-        'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Sarina&display=swap'
+        'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Sarina&family=Cotton:wght@400&display=swap'
     ];
     
     criticalResources.forEach(resource => {
